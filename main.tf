5eaa5f6442a84bd14403cc1715032bd53088e76f
gksankar7 <57542387+gksankar7@users.noreply.github.com>
2021-02-12 13:25:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ resource "random_id" "id" {
 resource "google_project" "project" {
   name            = var.project_name
   project_id      = random_id.id.hex
-  billing_account = var.billing_account
   org_id          = var.org_id
 }
 
